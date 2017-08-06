62b916ac0a71c81881b095774792113de7bae7e3
chenwei0601 <891336897@qq.com>
2017-08-06 16:24:10 +0800

    add

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ var driver = new webdriver.Builder().forBrowser('chrome').build();
 driver.get("http://www.baidu.com");//打开百度首页
 driver.findElement({id:"kw"}).sendKeys("chenwei");
 driver.findElement({id:"su"}).click();
+//driver.quit();
+
+driver.get("http://cn.bing.com/");
+driver.findElement({css:"#sb_form_q"}).sendKeys("陈薇");
+driver.findElement({css:"#sb_form_go"}).click();
